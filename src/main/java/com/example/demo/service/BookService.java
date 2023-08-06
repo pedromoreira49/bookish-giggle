9ccfaac524be8ca4f4be1d55e06f60cb88fb5f3c
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-08-06 20:27:00 -0300

    feat: adicionado service para book

---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class BookService {
+    
+    private BookRepository bookRepository;
+
+    public List<Book> findAll(){
+        return bookRepository.findAll();
+    }
+}
