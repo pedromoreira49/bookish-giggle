167d0747105f3cf42e5d81c3f1ff4b0223573265
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-08-06 20:26:33 -0300

    feat: adicionado model para book

---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Book {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String nome;
+
+}
