5668c66db2b5689d3a89f28887fb779eadc60874
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-08-07 01:06:48 -0300

    feat: alterado logica de Get e adicionado endpoint para post

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -2,7 +2,12 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Book;
@@ -12,13 +17,21 @@ import com.example.demo.service.BookService;
 import lombok.AllArgsConstructor;
 
 @RestController
+@RequestMapping("books")
 @AllArgsConstructor
 public class BookController {
     
     private BookService bookService;
 
     @GetMapping
-    public List<Book> findAll() {
-        return bookService.findAll();
+    public ResponseEntity<?> findAll() {
+        List<Book> books = bookService.findAll();
+        return ResponseEntity.ok(books);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Book book){
+        Book bookSaved = bookService.save(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookSaved);
     }
 }
